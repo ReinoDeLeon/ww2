78fbdedd18584db13250dde24d6db8a1e03a22b2
Iván <ivanantonsagastizabal@gmail.com>
2022-01-12 13:02:19 +0100

    Removed unnecesary imports

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,11 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "WW2 animation game";
